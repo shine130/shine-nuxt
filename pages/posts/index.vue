9eba736ebcea02a648535bc9fb22a69524d4836a
shine130 <shine130@vip.qq.com>
2019-05-03 15:08:03 +0800

    用vuex改造项目,完成 Nuxt.js：数据管理

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -3,15 +3,18 @@
     <h1 class="display-1 my-5">List</h1>
     <div class="row justify-content-center">
     <div class="col-md-6" v-for="post in posts" :key="post.id">
-      <nuxt-link :to="{name:'posts-id',params:{id:post.id}}">
         <div class="card my-3">
+          <nuxt-link :to="{name:'posts-id',params:{id:post.id}}">
           <img :src="post.imageUrl" :alt="post.title" class="card-img-top">
           <div class="cart-body">
             <h5 class="card-title">{{post.title}}</h5>
             <h6 class="card-subtitle mb-2 text-black-50">{{post.author}}</h6>
           </div>
+            </nuxt-link>
+            <div class="cart-footer">
+              <button @click="destroyAction(post.id)" class="btn btn-link">Delete</button>
+            </div>
         </div>
-      </nuxt-link>
     </div>
     </div>
   </div>
@@ -21,11 +24,24 @@
 import axios from 'axios';
 
 export default {
-  async asyncData(){
-    let {data} = await axios.get('http://localhost:3333/posts')
-    return {posts:data}
+  // async asyncData(){
+  //   let {data} = await axios.get('http://localhost:3333/posts')
+  //   return {posts:data}
+  // },
+  async fetch({store}){
+    const response = await axios.get('http://localhost:3333/posts')
+    store.commit('post/setList',response.data)
   },
-
+  computed:{
+    posts(){
+      return this.$store.state.post.list
+    }
+  },
+  methods:{
+    destroyAction(id){
+      this.$store.dispatch('post/destroyAction',id)
+    }
+  },
   head(){
     return {
       title:'Posts'
