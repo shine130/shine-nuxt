9b6ddfea873c8f77dd2ec7c56bbc6f343186cd5b
shine130 <shine130@vip.qq.com>
2019-05-03 14:11:51 +0800

    完成vuex 介绍

---
+++ store/demo.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+  pageName:'Vuex',
+  count:0
+})
+
+export const mutations = {
+  add(state,number){
+    state.count = state.count + number
+  }
+}
+
+export const actions = {
+  addAction(context,number){
+    setTimeout(() => {
+      context.commit('add', number)
+    },1000);
+  }
+}
